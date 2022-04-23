b84d63793da0acdb52f61f36439bd1dec2be6d35
Eron Alves <eron.alves@rocketmail.com>
2022-04-23 00:05:40 -0300

    Escrever função operate

--- main.js
+++ main.js
@@ -10,3 +10,21 @@ function multiply(firstOperand, secondOperand){
 function divide(firstOperand, secondOperand){
     return firstOperand / secondOperand;
 }
+function operate(firstOperand, secondOperand, operator){
+    switch (operator){
+        case '+':
+            return add(firstOperand,secondOperand);
+            break;
+        case '-':
+            return subtract(firstOperand,secondOperand);
+            break;
+        case '*':
+            return multiply(firstOperand, secondOperand);
+            break;
+        case '/':
+            return divide(firstOperand, secondOperand)
+            break;
+        default:
+            return "Invalid Operator!";
+    }
+}
