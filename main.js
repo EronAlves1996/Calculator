c11d0ecc7e8c6e46a9a75c9ab21e4bbfc1519ed0
Eron Alves <eron.alves@rocketmail.com>
2022-04-22 23:59:11 -0300

    Escrever a estrutura de funções Javascript

---
+++ main.js
@@ -0,0 +1,12 @@
+function add(firstOperand, secondOperand){
+
+}
+function subtract(firstOperand, secondOperand){
+    
+}
+function multiply(firstOperand, secondOperand){
+    
+}
+function divide(firstOperand, secondOperand){
+    
+}
